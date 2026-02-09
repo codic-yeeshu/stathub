8273d58db45c777757c49ee6414852ef0a28ee9a
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2026-02-09 18:51:45 +0530

    add arcjet for security and rate-limiting

--- server/src/utils/utils.js
+++ server/src/utils/utils.js
@@ -1,3 +1,8 @@
 export const logIt = (...args) => {
   if ( true ) console.log(...args);
 };
+
+export const logError = (...args) => {
+  if ( true ) console.log(...args);
+};
+
--- server/src/ws/server.js
+++ server/src/ws/server.js
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsSecurityMiddleware } from "../arcjet.js";
 
 function sendJson(socket, payload) {
   if (socket.readyState !== WebSocket.OPEN) return;
@@ -17,10 +18,33 @@ function broadcast(wss, payload) {
 export function attachWebSocketServer(server) {
   const wss = new WebSocketServer({ server, path: '/ws', maxPayload: 1024 * 1024 });
 
-  wss.on('connection', (socket) => {
+  wss.on('connection', async (socket, req) => {
+    const {success, code, reason} = await wsSecurityMiddleware(req) || {};
+    if (!success) {
+      socket.close(code, reason)
+      return;
+    }
+
+    // set the current socket as alive while connectiong and while reciving a pong heartbeat.
+    socket.isAlive = true
+    socket.on('pong', () => { socket.isAlive = true })
+
     sendJson(socket, { type: "welcome" })
 
     socket.on('error', console.error)
+  })
+
+  const checkAliveStatus = setInterval(() => {
+    wss.clients.forEach(client => {
+      if (client.isAlive === false) return client.terminate();
+      client.isAlive = false
+      client.ping()
+    });
+
+  }, 30 * 1000);
+
+  wss.on('close', () => {
+    clearInterval(checkAliveStatus)
   })
 
   function broadcastMatchCreated(match) {
